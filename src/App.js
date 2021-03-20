7f9b31b26361be375776a29e284d9db49967ab85
Richard Obiri <obiriaframson@gmail.com>
2021-03-20 01:33:03 +0000

    try

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import Peer from "simple-peer"
 import io from "socket.io-client"
 import "./App.css"
 
-const socket = io.connect('https://avivid.herokuapp.com/')
+const socket = io.connect('ws://avivid.herokuapp.com/')
 
 
 function App() {
